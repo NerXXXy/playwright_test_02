b5dc6496c454c46a4f441ae8c01492d24409ee47
NerXXXy <nerxxxxy@gmail.com>
2022-07-17 15:02:30 +0300

    devices (  library  )

---
+++ 13_emulateDevice.js
@@ -0,0 +1,12 @@
+const {chromium,devices} = require('playwright');
+
+(async () => {
+
+    const browser = await chromium.launch({headless:false})
+    const iPhone11 = devices["iPhone 11 Pro"];
+    const context = await browser.newContext({...iPhone11})
+    const page = await context.newPage()
+
+    await page.goto('https://www.google.com')
+
+}) ()
