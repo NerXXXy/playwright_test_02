c757e73cc9a4bc924cff440a38a87b8f6d1eba6b
NerXXXy <nerxxxxy@gmail.com>
2022-07-17 14:56:49 +0300

    permissions, grantPermissions,clearPermissions, geolocation, setGeolocation

---
+++ 12_permissions.js
@@ -0,0 +1,20 @@
+const {chromium} = require('playwright');
+
+(async () => {
+
+    const browser = await chromium.launch({headless:false})
+    const context = await browser.newContext({
+        geolocation:{longitude:24.02615547180176, latitude:49.843988271866735},
+        permissions:['geolocation'],
+    })
+
+    // await context.grantPermissions(['geolocation'])
+    // await context.clearPermissions()
+
+    // await context.setGeolocation({longitude:24.02615547180176, latitude:49.843988271866735})
+
+    const page = await context.newPage()
+    await page.goto('https://www.maps.ie/coordinates.html')
+    await page.click('#find-loc')
+
+}) ()
